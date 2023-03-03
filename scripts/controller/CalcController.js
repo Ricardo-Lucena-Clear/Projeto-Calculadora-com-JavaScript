e8fc0612d80daf3b13e23cdc30b94046b16a7987
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-03 09:00:49 -0300

    This,encapsulation, getters and setters

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,1 +1,26 @@
-class CalcController {}
+class CalcController {
+    constructor(){
+
+
+        this.displayCalc = "0";
+        this.dataAtual;
+
+    }
+    
+    get displayCalc(){
+    return this._displayCalc;
+}
+
+
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    get dataAtual(){
+        return this._dataAtual;
+    }
+    set dataAtual(valor){
+        this._dataAtual = valor;
+    }
+  
+}
