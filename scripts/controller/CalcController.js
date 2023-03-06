8ec2022a2b77e3c44cc74e548b585c9a1df876e4
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-06 08:57:06 -0300

    Adding FOR and looking for the last number of the ARRAY

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -61,13 +61,24 @@ class CalcController {
     calc(){
         let last = this._operation.pop();
 
-        let result = eval (this._operation.join());
+        let result = eval (this._operation.join(""));
 
         this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
     }
     setLastNumberToDisplay(){
+        let lastNumber; 
+        for (let i = this._operation.length-1; i >= 0; i--){
+            if (!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
 
-        
+            }
+        }
+        this.displayCalc = lastNumber;
+
+
     }
     addOperation(value){
         console.log('A',isNaN(this.getLastOperation()));
@@ -77,6 +88,7 @@ class CalcController {
             } else if (isNaN(value)) {
             }else {
                 this.pushOperator(value);
+                this.setLastNumberToDisplay();
             }
         }
 
