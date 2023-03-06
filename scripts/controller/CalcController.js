c5e9b5afeadd725d866633db174dc49b362d6a9a
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-06 12:50:56 -0300

    Working with audio

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,7 @@
 class CalcController {
     constructor(){
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -41,9 +43,27 @@ class CalcController {
 
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+            btn.addEventListener('dblclick', e=>{
+                this.toggleAudio();
+
+            })
+        })
+    }
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+    playAudio(){
+        if (this._audioOnOff){
+            this._audio.play();
+
+        }
     }
     initKeyboard(){
         document.addEventListener('keyup', e =>{
+            this.playAudio();
             switch (e.key) {
 
                 case 'Escape':
@@ -240,6 +260,7 @@ class CalcController {
     }
 
     execBtn(value){
+        this.playAudio();
         switch (value) {
 
             case 'ac':
